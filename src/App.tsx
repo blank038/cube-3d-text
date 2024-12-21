80f01a34078a3613d7a6136f29f1a5cffe1e12e4
boybook <fangyizhou1997@qq.com>
2024-12-21 21:57:35 +0800

    修复字体切换

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const fontsMap: {[name: string]: string} = {
     "REEJI Taiko Magic": "font/REEJI-TaikoMagicGB-Flash_Regular.json",
     "汉仪力量黑(简)": "font/HYLiLiangHeiJ_Regular.json",
     "Minecraft Ten": "font/Minecraft_Ten_Regular.json",
-    "Fusion Pixel 8px": "font/Fusion_Pixel_8px_Proportional zh_hans_Regular.json",
+    "Fusion Pixel 8px": "font/Fusion_Pixel_8px_Proportional_zh_hans_Regular.json",
     "Fusion Pixel 10px": "font/Fusion_Pixel_10px_Proportional_zh_hans_Regular.json",
 };
 
